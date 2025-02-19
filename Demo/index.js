72a47c669b3dc663546c74ff0a8a4cc23701919a
Renzo Kooi <renzo.kooi@gmail.com>
2025-02-19 13:23:47 +0100

    more demo

--- Demo/index.js
+++ Demo/index.js
@@ -7,26 +7,6 @@ window.$ = $;
 initialize();
 const initialCode = toCodeBlock(templates.find$(`#initial`).HTML.get().trim());
 const performanceCode = toCodeBlock(templates.find$(`#perf`).HTML.get().trim())
-$.delegate(`click`, `#bttnOpenClose, details.chapter`, evt => {
-  if (evt.target.closest(`#bttnOpenClose`)) {
-    const theBttn = evt.target;
-    const allOpen = theBttn.dataset?.allopen === '1' ?? false;
-    $(`details.chapter`).each(el => {
-      el.open = !allOpen;
-    });
-    return theBttn.dataset.allopen = allOpen ? `0` : `1`;
-  }
-  
-  if (evt.target.closest(`.chapter`)) {
-    setTimeout(() => {
-      const theDetailsElements = $.nodes(`details.chapter`).filter(el => el.open);
-      const theBttn = $.node(`#bttnOpenClose`);
-      theBttn.dataset.allopen = theDetailsElements.length ? `1` : `0`;
-    });
-    
-    return;
-  }
-});
 
 const browserTZ = $D.localeInformation.timeZone;
 const browserLocale = $D.localeInformation.locale;
@@ -35,7 +15,7 @@ const chongqin = $D({l: `zh`, timeZone: "Asia/Chongqing"});
 const auckland = $D({timeZone: "Pacific/Auckland"});
 const vancouver = now$.clone.relocate({locale: `en-CA`, timeZone: `America/Vancouver`});
 const paris = $D({timeZone: "Europe/Paris"}).add("2 days, 3 hours, 22 minutes");
-const berlin = $D({timeZone: "Europe/Berlin"});
+const berlin = $D({l: "de-DE", tz: "Europe/Berlin"});
 const la = $D({timeZone: "America/Los_Angeles"});
 const taiohae = $D.from(2025, 0, 1, 7, 0, 0);
 const utc = $D.now.UTC;
@@ -51,7 +31,7 @@ print(
   
   `<button id="bttnOpenClose" data-allopen="0"></button>`,
   
-  toDetailChapter(`locale/timeZone`,
+  toDetailChapter(`locale/timeZone`, `ltz`,
     toDetailsBlock(
       `<code>$D.localeInformation</code>: environment (here: browser) locale- and timeZone information`,
       toJSONString($D.localeInformation, true)),
@@ -64,17 +44,56 @@ print(
       `<code>chongqin.localeInfo</code> (chinese locale, Chongqing timeZone)`,
       toJSONString(chongqin.localeInfo, true))),
   
-  toDetailChapter(`toString`,
+  toDetailChapter(`toString`, `tostring`,
     toDetailsBlock(
       `<code>chongqin.toString()</code> (Chongqing timeZone)`,
       chongqin.toString(), true),
     
     toDetailsBlock(
       `<code>chongqin.<span class="red">value</span>.toString()</code> (<i>your</i> timeZone)`,
-      chongqin.value.toString(), true)
+      chongqin.value.toString(), true),
+    
+    toDetailsBlock(
+      `<code>chongqin.toString("WD d MM yyyy")</code> (see Format)`,
+      chongqin.toString("WD d M yyyy"), true),
   ),
   
-  toDetailChapter(`Difference`,
+  toDetailChapter(`Names`, `names`,
+    toDetailsBlock(
+      `<code>$D.localMonthnames("es-CL").long.slice(0, 3).join(" / ")</code>`,
+      $D.localMonthnames("es-CL").long.slice(0, 3).join(` / `) ),
+    
+    
+    toDetailsBlock(
+      `<code>berlin.dayName</code>/<code>berlin.zoneDayname</code>`,
+      `${berlin.dayName}/${berlin.zoneDayname}`),
+    
+    toDetailsBlock(
+      `<code>chongqin.monthName</code>/<code>chongqin.zoneMonthname</code>`,
+      `${chongqin.monthName}/${chongqin.zoneMonthname}`),
+    
+    toDetailsBlock(
+      `<code>chongqin.names</code>`,
+      toJSONString(chongqin.names)),
+    
+    toDetailsBlock(
+      `<code>chongqin.<span class="red">zone</span>Names.monthNames.long</code>`,
+      toJSONString(chongqin.zoneNames.monthNames.long)),
+    
+    toDetailsBlock(
+      `<code>paris.relocate({l:"fr"}).zoneNames.dayNames.long</code>`,
+      toJSONString(paris.relocate({l: "fr"}).zoneNames.dayNames.long)),
+    
+    toDetailsBlock(
+      `<code>paris.relocate({l:"ar-DZ"}).zoneNames.monthNames.long</code>`,
+      toJSONString(paris.relocate({l: "ar-DZ"}).zoneNames.monthNames.long)),
+    
+    toDetailsBlock(
+      `<code>$D.localMonthnames("fr")</code>`,
+      toJSONString($D.localMonthnames("fr")) ),
+  ),
+  
+  toDetailChapter(`Difference`, `difference`,
     toDetailsBlock(`<code>chongqin.differenceTo(auckland)</code>`,
       toJSONString(chongqin.differenceTo(auckland), true)),
     
@@ -94,29 +113,29 @@ print(
       toJSONString(paris.differenceTo(berlin), true))
   ),
   
-  toDetailChapter(`Date/time values`,
+  toDetailChapter(`Date/time values`, `dtvalues`,
     toDetailsBlock("<code>auckland.dateTime</code>", toJSONString(auckland.dateTime)),
     
-    toDetailsBlock("<code>auckland.zoneDateTime</code>", toJSONString(auckland.zoneDateTime)),
+    toDetailsBlock("<code>auckland.<span class='red'>zone</span>DateTime</code>", toJSONString(auckland.zoneDateTime)),
     
     toDetailsBlock("<code>taiohae.zoneDateTime</code> (<b>note</b>: UTC offset -9:30)",
       toJSONString(taiohae.zoneDateTime))
   ),
   
-  toDetailChapter(`Offset (from)`,
+  toDetailChapter(`Offset (from)`, `offset`,
     toDetailsBlock("<code>taiohae.offsetFrom(now$)</code>", toJSONString(taiohae.offsetFrom(now$)), ),
     toDetailsBlock("<code>la.offsetFrom(auckland)</code>", toJSONString(la.offsetFrom(auckland)), ),
     toDetailsBlock("<code>auckland.offsetFrom(la)</code>", toJSONString(auckland.offsetFrom(la)), ),
     toDetailsBlock("<code>utc.offsetFrom(la)</code>", toJSONString(utc.offsetFrom(la)), ),
   ),
   
-  toDetailChapter(`Info`,
+  toDetailChapter(`Info`, `info`,
     toDetailsBlock("<code>taiohae.info</code>", toJSONString(taiohae.info), ),
     toDetailsBlock("<code>chongqin.info</code>", toJSONString(chongqin.info), ),
     toDetailsBlock("<code>now$.info</code>", toJSONString(now$.info), )
   ),
   
-  toDetailChapter(`Format`,
+  toDetailChapter(`Format`, `format`,
     `<div>See <a target="_blank" href="https://github.com/KooiInc/dateformat">[GitHub]dateformat</a> for syntax</div>`,
     toDetailsBlock("<code>auckland.zoneFormat(...)</code> formats to instance embedded locale/timeZone",
       `<code>auckland.zoneFormat('{=>; in Auckland it\'s now} WD d MM yyyy, hh:mmi:ss dp')</code>` +
@@ -135,9 +154,9 @@ print(
   ),
 );
 const perf = perfRunner();
-print(toDetailChapter(`Performance`,
-  `<div><b class="warn">Note</b>: do <b><i class="warn">not</i></b> use TickTock.js (or use selectively)
-      if you need to process tens of thousands of Dates &#128128;</div>`,
+print(toDetailChapter(`Performance`, false,
+  `<div><b class="warn">Note</b>: consider <b><i class="warn">not</i></b> (or selectively) using TickTock.js
+      for processing a gazillion Dates &#128128;</div>`,
   performanceCode, perf.join(`<br>`)));
 
 Prism.highlightAll();
@@ -151,10 +170,15 @@ function toJSONString(obj, detail = true) {
   return `<pre${detail ? ` class="detail"` : ``}>${JSON.stringify(obj, null, 2)}</pre>`;
 }
 
-function toDetailChapter(summary, ...lemmas) {
+function toDetailChapter(summary, id, ...lemmas) {
   return `
-    <details class="chapter">
-      <summary><span><b>${summary}</b></span></summary>
+    <details class="chapter" ${id ? id="${id}" : ""}>
+      <summary>
+        <span>
+          <b>${summary}</b>
+          ${id ? `<button data-close="${id}">Close all below</button>` : ``}
+        </span>
+      </summary>
       ${lemmas.join(``)}
     </details>`;
 }
@@ -204,13 +228,13 @@ function initialize() {
       width: 900px;
       
       @media screen and (width < 900px) {
-        max-width: 700px;
+        max-width: 720px;
       }
     }`,
     `code:not(.language-javascript) {
       background-color: rgb(227, 230, 232);
       color: rgb(12, 13, 14);
-      padding: 0 4px;
+      padding: 2px 4px;
       display: inline-block;
       border-radius: 4px;
       margin: 1px 0;
@@ -220,35 +244,36 @@ function initialize() {
     }`,
     `details {
        cursor: pointer;
-       summary {
-         vertical-align: top;
-       }
        &.chapter {
           &:open {
             summary {
+              button { display: inline-block; }
               color: green;
               list-style: inside disclosure-open;
               span:not(.red) {
-                background-color: lightsteelblue;
-                color: cornsilk;
+                background-color: #6196cc;
+                color: floralwhite;
+                border-radius: 3px;
+                display: inline-block;
               }
             }
           }
           summary {
+            button { display: none; }
             font-family: monospace;
             font-size: 1.2rem;
             font-weight: bold;
             color: black;
             margin-bottom: 0.5rem;
-            padding: 0.1rem;
+            padding: 0;
             span:not(.red) {
-              padding: 1px 4px;
+              padding: 2px 4px;
               &:hover {
-                background-color: #eee;
-                color: #777;
+                background-color: #6196cc;
+                color: floralwhite;
+                border-radius: 3px;
               }
             }
-            
           }
           
           details:not(.chapter) {
@@ -288,13 +313,14 @@ function initialize() {
       margin: 0.7rem 0px 0px -1.2rem;
       padding-left: 0;
     }`,
-    `.red { color: red; }`,
+    `.red { color: red; font-weight: bold; }`,
     `#log2screen li div {
       font-weight: normal;
       color: darkolivegreen;
-      max-width: 90%;
+      max-width: 95%;
       h3 { color: black; margin: 0; margin-top: 0.2rem !important; }
       div {
+        max-width: 95%;
         margin: 0.4rem 0;
 
         code.block {
@@ -311,6 +337,49 @@ function initialize() {
       code { color: inherit; }
     }`
   );
+  $.delegate(`click`, `#bttnOpenClose, details.chapter, button[data-close]`, evt => {
+    const isLemmaCloser = evt.target.dataset.close;
+    const mainBttn = evt.target.closest(`#bttnOpenClose`);
+    const lemma = evt.target.closest(`details:not(.chapter)`);
+    const chapter = evt.target.closest(`.chapter`);
+    
+    if (mainBttn) {
+      const allOpen = mainBttn.dataset?.allopen === '1' ?? false;
+      $(`details.chapter`).each(el => {
+        el.open = !allOpen;
+        
+        if(!el.open) {
+          $(el).find(`details`).forEach(dt => dt.open = false);
+        }
+      });
+      
+      return mainBttn.dataset.allopen = allOpen ? `0` : `1`;
+    }
+    
+    if (isLemmaCloser) {
+      evt.preventDefault();
+      $(evt.target.closest(`.chapter`))
+        .find$(`details`).each(dt => dt.open = false);
+      
+      return true;
+    }
+    
+    if (lemma) {
+      return true;
+    }
+    
+    if (chapter) {
+      setTimeout(() => {
+        $(chapter).find(`details`).forEach(el => el.open = chapter.open);
+        const theDetailsElements = $.nodes(`details.chapter`).filter(el => el.open);
+        const theBttn = $.node(`#bttnOpenClose`);
+        theBttn.dataset.allopen = theDetailsElements.length ? `1` : `0`;
+      });
+      
+      return;
+    }
+    return true;
+  });
 }
 
 async function fetchTemplates() {
