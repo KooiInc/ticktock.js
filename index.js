ad77181cb40bbbdb911a3627241514a393f3379a
Renzo Kooi <renzo.kooi@gmail.com>
2025-02-26 12:22:04 +0100

    main: needs no factory

--- index.js
+++ index.js
@@ -8,19 +8,16 @@ import {
 } from "./src/genericHelpers.js";
 
 const customMethods = {};
-const ctor = XDateFactory()
 createCTORStaticMethods(ctor, customMethods);
 
 export default ctor;
 
-function XDateFactory() {
-  return function(input, localeInfo) {
-    const inputIsLocaleInfo = input?.locale || input?.timeZone || input?.tz || input?.l;
-    let maybeDate = new Date(inputIsLocaleInfo ? Date.now() : retrieveDateValueFromInput(input));
-    const localeInfoResolved = inputIsLocaleInfo
-      ? localeInfoValidator(input) : localeInfoValidator(localeInfo || {});
-    const instanceExtensions = instanceCreator({localeInfo: localeInfoResolved});
-    const instance = instanceExtensions.proxy(maybeDate, getTraps(instanceExtensions));
-    return Object.freeze(instance.addAggregates(instance, getAggregates(instance, customMethods)));
-  }
+function ctor(input, localeInfo) {
+  const inputIsLocaleInfo = input?.locale || input?.timeZone || input?.tz || input?.l;
+  let maybeDate = new Date(inputIsLocaleInfo ? Date.now() : retrieveDateValueFromInput(input));
+  const localeInfoResolved = inputIsLocaleInfo
+    ? localeInfoValidator(input) : localeInfoValidator(localeInfo || {});
+  const instanceExtensions = instanceCreator({localeInfo: localeInfoResolved});
+  const instance = instanceExtensions.proxy(maybeDate, getTraps(instanceExtensions));
+  return Object.freeze(instance.addAggregates(instance, getAggregates(instance, customMethods)));
 }
